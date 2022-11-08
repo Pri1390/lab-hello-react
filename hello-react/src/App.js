9ecffff8e7d44b953026418b0880338bbbddb4a8
Priscila Vieira <freedom.pva@gmail.com>
2022-11-07 21:31:26 -0300

    done

---
+++ hello-react/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import logo from './images/ironhack-logo-xs.png'
+import menu from './images/menu-top-xs.png'
+import icon1 from './images/icon1.png'
+import icon2 from './images/icon2.png'
+import icon3 from './images/icon3.png'
+import icon4 from './images/icon4.png'
+
+function App() {
+  return (
+    <div className="App"></div>
+  );
+}
+
+export default App;
